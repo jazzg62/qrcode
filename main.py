1eb32b4013f2eee1bfbe85d5743b347045735113
jazzg62.com <z123zxc2@gmail.com>
2021-10-20 08:55:52 +0800

    1

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
 from PIL import Image,ImageDraw, ImageFont
 import qrcode
 import json
+import os
+import threading
+
+thread_lock = threading.BoundedSemaphore(value=3)
 
 def addtext(img, text):
     draw = ImageDraw.Draw(img)
     ttfront = ImageFont.truetype('simhei.ttf', 125)#字体大小
     draw.text((1860, 4275),text,fill=(0,25,25), font=ttfront)#文字位置，内容，字体
-    # img.show()
 
 def addqrcode(img, text):
     qrcode_img = qrcode.make(text, border = 0)
-    print(qrcode_img.size)
     qrcode_img = qrcode_img.resize((1550,1450))
-    print(qrcode_img.size)
 
     origin = (1485,2780,1485+qrcode_img.size[0],2780+qrcode_img.size[1])
     img.paste(qrcode_img, origin)
 
+def createImg(sn):
+    base_img = Image.open('./qrcode_bg_sn.png')
+    qrcode_url =  "https://pay.cnqilian.com/dist/?name=store&sn=" + sn
+    addtext(base_img, 'NO.'+sn)
+    addqrcode(base_img, qrcode_url)
+    base_img.save('./output/'+sn+'.png')
+    thread_lock.release()
+
 # 读取模板图片
 base_img = Image.open('./qrcode_bg_sn.png')
-# print(base_img.size)  # (4501, 6751)
 
-# sn = '1026730337'
-# qrcode_url =  "https://pay.cnqilian.com/dist/?name=store&sn=" + sn
-# addtext(base_img, 'NO.'+sn)
-# addqrcode(base_img, qrcode_url)
-# base_img.show()
+# 创建存储文件夹
+if os.path.exists('output') is not True:
+    os.mkdir('output')
 
-with open('./qrcode.json','r') as f:
+with open('./1.json','r') as f:
     t = f.read()
     list = json.loads(t)
-    print(list['RECORDS'][0])
-    print(type(list['RECORDS'][0]['bh']))
-
+    
     records = list['RECORDS']
-    for i in range(0,1000):
-        print(records[i])
-        base_img = Image.open('./qrcode_bg_sn.png')
-        sn = str(records[i]['bh'])
-        qrcode_url =  "https://pay.cnqilian.com/dist/?name=store&sn=" + sn
-        addtext(base_img, 'NO.'+sn)
-        addqrcode(base_img, qrcode_url)
-        base_img.save('./output/'+sn+'.png')
-        # base_img.show()
+    for i in range(0,len(records)):
+        print('第{}条记录'.format(i))
+        thread_lock.acquire()  # 线程锁
+        T = threading.Thread(target = createImg, args=(str(records[i]['bh']), ))
+        T.start()
+        # createImg(str(records[i]['bh']))
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
 # 批量生成收款码
+
+## 安装
+> pip install Pillow
+
+> pip install qrcode
+
+## 运行
+```bash
+python main.py
+```
